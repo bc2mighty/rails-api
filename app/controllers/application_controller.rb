1dbd5fbf4748523649461781389b9969f8fe0aa9
bc2mighty <akinadetunmise8@gmail.com>
2021-11-21 21:42:26 +0100

    Exception for action controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  def not_found(e)
+    return render json: {status: "error", message: "Record with id: #{params[:id]} not found"}, status: :unprocessable_entity
+  end
+
+  def not_destroyed(e)
+    return render json: {status: "error", message: "record could not be destroyed", errors: e.record.errors }, status: :unprocessable_entity
+  end
 end
