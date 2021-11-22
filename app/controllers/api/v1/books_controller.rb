98e014b6f62d7bbcf6c4f9142e0bbfdda2f14c42
bc2mighty <akinadetunmise8@gmail.com>
2021-11-22 09:03:55 +0100

    Added Representers to controllers

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,8 +1,11 @@
 module Api
   module V1
+    require './app/representers/books_representer'
+
     class BooksController < ApplicationController
       def index
-        render json: Book.all
+        books = Book.all
+        render json: BooksRepresenter.new(books).as_json
       end
 
       def create
