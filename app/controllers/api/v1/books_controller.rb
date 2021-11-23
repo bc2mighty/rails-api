ad6a23050f3f92b3a1019ac500cd948fe8960bee
bc2mighty <akinadetunmise8@gmail.com>
2021-11-23 10:02:24 +0100

    Created Controller specific tests

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -3,8 +3,10 @@ module Api
     require './app/representers/books_representer'
 
     class BooksController < ApplicationController
+      MAX_PAGINATION_LIMIT = 100
+
       def index
-        books = Book.all
+        books = Book.limit(limit).offset(params[:offset])
         render json: BooksRepresenter.new(books).as_array_json
       end
 
@@ -25,6 +27,13 @@ module Api
       end
 
       private
+
+      def limit
+        [
+          params.fetch(:limit, MAX_PAGINATION_LIMIT).to_i,
+          MAX_PAGINATION_LIMIT
+        ].min
+      end
 
       def book_params
         params.require(:book).permit(:title, :author_id)
--- spec/requests/book_spec.rb
+++ spec/requests/book_spec.rb
@@ -26,6 +26,44 @@ describe 'Books API', type: :request do
         }
       ])
     end
+
+    it 'returns a subset of books based on limit' do
+      author = FactoryBot.create(:author, first_name: "Shade", last_name: "Blade", age: 45)
+      FactoryBot.create(:book, title: "Test Title 1", author: author)
+      FactoryBot.create(:book, title: "Test Title 2", author: author)
+
+      get '/api/v1/books', params: { limit: 1}
+
+      expect(response).to have_http_status(:success)
+      expect(response_body.size).to eq(1)
+      expect(response_body).to eq([
+        {
+          "id" => 1,
+          "title" => "Test Title 1",
+          "author_name" => "Shade Blade",
+          "author_age" => 45,
+        }
+      ])
+    end
+
+    it 'returns a subset of books based on limit and offset' do
+      author = FactoryBot.create(:author, first_name: "Shade", last_name: "Blade", age: 45)
+      FactoryBot.create(:book, title: "Test Title 1", author: author)
+      FactoryBot.create(:book, title: "Test Title 2", author: author)
+
+      get '/api/v1/books', params: { limit: 1, offset: 1}
+
+      expect(response).to have_http_status(:success)
+      expect(response_body.size).to eq(1)
+      expect(response_body).to eq([
+        {
+          "id" => 2,
+          "title" => "Test Title 2",
+          "author_name" => "Shade Blade",
+          "author_age" => 45,
+        }
+      ])
+    end
   end
   
   describe "POSTS /books" do
